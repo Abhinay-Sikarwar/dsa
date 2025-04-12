5c470f2aa5d7522565403291862e2291626a6350
abhinay sikarwar <work.abhinaysikarwar@gmail.com>
2025-04-12 12:44:26 +0530

    First commit

---
+++ ARRAY & STRINGS.java
@@ -0,0 +1,36 @@
+// 1. ARRAYS & STRING
+// EASY
+     
+    // 1: TWO SUM
+// BRUTE FORCE 
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            int n = nums.length;
+            for(int i = 0; i < n - 1; i++){
+                for(int j = i + 1; j < n; j++){
+                    if(nums[i] + nums[j] == target){
+                        return new int[]{i,j};
+                    }
+                }
+            }
+            return new int[]{};//case where target not found
+        }
+    }
+ 
+// ONE-PASS HASHTABLE
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            Map<Integer, Integer> numMap = new HashMap<>();
+            int n = nums.length;
+    
+            for (int i = 0; i < n; i++) {
+                int complement = target - nums[i];
+                if (numMap.containsKey(complement)) {
+                    return new int[] { numMap.get(complement), i };
+                }
+                numMap.put(nums[i], i);
+            }
+    
+            return new int[] {};//when target is not found
+        }
+    }
