62d035e7c6c0503b9cee2b43518735a7fbe22d16
abhinay sikarwar <work.abhinaysikarwar@gmail.com>
2025-04-21 22:59:33 +0530

    twenty-fifth commit

--- BINARY SEARCH & SORTING.java
+++ BINARY SEARCH & SORTING.java
@@ -3,4 +3,30 @@
 // EASY
 
     // 278: FIRST BAD VERSION
-//      
+// BINARY SEARCH
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+    public class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            int l = 1;
+            return findBad(l,n);
+        }
+    
+        private int findBad(int left, int right) {
+            if (left == right) {
+                return left;
+            }
+    
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                return findBad(left, mid);
+            } else {
+                return findBad(mid + 1, right);
+            }
+        }
+    }
+    
+    // ⏱️ Time Complexity:  O(logn)
+    // 🧠 Space Complexity:  O(logn) 
+
