f8ce5042fbd0072e70eb6dc99a60870b58dfd8ea
abhinay sikarwar <work.abhinaysikarwar@gmail.com>
2025-04-22 23:06:04 +0530

    twenty-seventh commit

--- BINARY SEARCH & SORTING.java
+++ BINARY SEARCH & SORTING.java
@@ -54,3 +54,45 @@
     
     // ⏱️ Time Complexity:  O(logn)
     // 🧠 Space Complexity:  O(1)
+
+    // MEDIUM
+
+    // 34: FIND FIRST AND LAST POSITION OF ELEMENT IN SORTED ARRAY
+// BINARY SEARCH WITH SOME TWEAKS
+    class Solution {
+        public int[] searchRange(int[] nums, int target) {
+            if (nums.length == 0 || target < nums[0] || target > nums[nums.length - 1]) {
+                return new int[] {-1, -1};
+            }
+            int left = binary_search(nums, target, true);
+            if (left == -1) return new int[] {-1, -1};
+            int right = binary_search(nums, target, false);
+            return new int[] {left, right};
+        }
+
+        private int binary_search(int[] nums, int target, boolean isSearchingLeft) {
+            int left = 0, right = nums.length - 1, index = -1;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+
+                if (nums[mid] > target) {
+                    right = mid - 1;
+                } else if (nums[mid] < target) {
+                    left = mid + 1;
+                } else {
+                    index = mid;
+                    if (isSearchingLeft) {
+                        right = mid - 1;
+                    } else {
+                        left = mid + 1;
+                    }
+                }
+            }
+
+            return index;
+        }
+    }
+    
+    // ⏱️ Time Complexity:  O(logn)
+    // 🧠 Space Complexity:  O(1)
