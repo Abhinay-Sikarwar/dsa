af141ff934554f599a0b2050015271311f620439
abhinay sikarwar <work.abhinaysikarwar@gmail.com>
2025-04-26 23:12:38 +0530

    thirty-fourth commit

--- TWO POINTERS & SLIDING WINDOW.java
+++ TWO POINTERS & SLIDING WINDOW.java
@@ -74,3 +74,41 @@
     
     // ⏱️ Time Complexity:  O(n)
     // 🧠 Space Complexity:  O(n)
+
+// MEDIUM
+ 
+    // 16: 3SUM CLOSEST
+// TWO POINTERS, SORTING
+    class Solution {
+        public int threeSumClosest(int[] nums, int target) {
+            Arrays.sort(nums);
+            int closestSum = nums[0] + nums[1] + nums[2];
+
+            for (int i = 0; i < nums.length - 2; i++) {
+                int j = i + 1;
+                int k = nums.length - 1;
+                if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+                while (j < k) {
+                    int sum = nums[i] + nums[j] + nums[k];
+
+                    if (sum == target) return sum;
+
+                    if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+                        closestSum = sum;
+                    }
+
+                    if (sum < target) {
+                        j++;
+                    } else {
+                        k--;
+                    }
+                }
+            }
+
+            return closestSum;
+        }
+    }
+    
+    // ⏱️ Time Complexity:  O(n^2)
+    // 🧠 Space Complexity:  O(1)
